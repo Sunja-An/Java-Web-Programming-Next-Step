de737b52df1a4a9038750603a5875588372dfad6
Sunwoo-An <110546006+Sunja-An@users.noreply.github.com>
2025-03-17 22:23:57 +0900

    Feat: First Setting

---
+++ src/main/java/com/study/nextprogamming/NextprogammingApplication.java
@@ -0,0 +1,13 @@
+package com.study.nextprogamming;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NextprogammingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NextprogammingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/study/nextprogamming/NextprogammingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.nextprogamming;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NextprogammingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
